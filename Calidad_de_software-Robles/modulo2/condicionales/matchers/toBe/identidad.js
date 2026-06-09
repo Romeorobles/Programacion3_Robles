d33a3f883889a7936d58229ce9ab6ecc60226d09
Romeorobles <nicolrobles423@gmail.com>
2026-06-09 08:56:37 -0500

    Commit  calidad de software  del dia 09/06/2026

---
+++ Calidad_de_software-Robles/modulo2/condicionales/matchers/toBe/identidad.js
@@ -0,0 +1,18 @@
+function identidad(valor, { clone = false } = {}) {
+    const esObjeto = valor !== null && typeof valor === 'object';
+    if (clone) {
+        if (!esObjeto) throw new TypeError('Solo se puede clonar objetos');
+        if (Array.isArray(valor)) return [...valor];
+        return { ...valor };
+    }
+    return valor;
+}
+
+function suma(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError('a y b deben ser números');
+    }
+    return a + b;
+}
+
+module.exports = { identidad, suma };
